eb1a10bcea9bfcebe124208f37dc7e8a6dcfd9c2
Annemoon_de_Groen <annemoon2003@gmail.com>
2024-02-15 13:40:29 +0100

    Finished exercise

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,6 +2,22 @@ const answer = []
 
 // Write your code below this line
 
+for (let i = 1; i < 16; i++)
+{
+    if (i % 5 === 0 && i % 3===0) {
+        answer.push('FizzBuzz')
+        continue
+    }
+    if (i % 5 === 0) {
+        answer.push('Buzz')
+        continue
+    }
+    if (i % 3 === 0) {
+        answer.push('Fizz')
+        continue
+    }
+    answer.push(i)
+}
 
 
 
